7fff77d6ec40ad0d7ab5e97b9116a6049c6f6d27
Amaka Atoyebi <amaka.atoyebi@gmail.com>
2013-08-22 00:04:45 -0400

    Sprint 3 commits

--- chap8.py
+++ chap8.py
@@ -1,2 +1,12 @@
 # This is where the answers to Chapter 8 questions for the BSS Dev RampUp go
-# Name:
+# Name: Amaka Atoyebi
+
+#8.12
+def rotate_word(string, integer):
+	letters = string.split()
+	for letters in string:
+		letterToNum = ord(letters) + integer
+		numToLetter = chr(letterToNum)
+		print numToLetter
+
+rotate_word('America', 13)	 
--- chap9.py
+++ chap9.py
@@ -1,2 +1,57 @@
 # This is where the answers to Chapter 9 questions for the BSS Dev RampUp go
-# Name:
+# Name: Amaka Atoyebi
+
+#9.1
+readFile = open('words.txt')
+line = readFile.readline()
+for line in readFile:
+	if (len(line) > 20):
+		word = line.strip()
+		print word
+
+
+#9.2
+
+def has_no_e():
+	counter = 0
+	counter_all = 0
+	for line in readFile:
+		if 'e' in line:
+			word = line.strip()
+			counter += 1
+			print word
+		counter_all += 1
+	num = counter * 100
+	den = counter_all
+	print num / den
+	
+	return True
+
+
+has_no_e()
+
+
+#9.3
+def avoids():
+	prompt = "Please enter your word:\n"
+	my_input = raw_input(prompt)
+	counter = 0
+	for line in readFile:
+		if my_input not in line:
+			word = line.strip()
+			counter += 1
+			print word
+	print counter
+			
+	return True
+
+avoids()
+
+#9.4
+def uses_only(word, letters):
+	for letters in word:
+		if letters in word:
+			return True
+
+uses_only('Hello Ace Flo', 'aceflho')
+
